6095a5876d374c0e3d0c65eb991d5f01883af37f
Luul1649 <luulhaji1649@gmail.com>
2026-03-04 11:24:28 +0300

    Update app.py

--- app.py
+++ app.py
@@ -52,7 +52,14 @@ plt.plot(ma_100_days,'r')
 plt.plot(data.Close,'g')
 plt.show()
 st.pyplot(fig2)
-
+st.header('Price vs MA50 vs MA100 vs MA200')
+ma_200_days=data.Close.rolling(200).mean()
+fig3=plt.figure(figsize=(8,6))
+plt.plot(ma_100_days,'r')
+plt.plot(ma_200_days,'r')
+plt.plot(data.Close,'g')
+plt.show()
+st.pyplot(fig3)
 x=[]
 y=[]
 for i in range(100,data_test_scaler.shape[0]):
@@ -64,6 +71,7 @@ predict=model.predict(x)
 scale=1/scaler.scale_
 predict=predict*scale
 y=y*scale
+
 
 
 
